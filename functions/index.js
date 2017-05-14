8ee95498c5b2d80e56638924a5c2edd01f91d139
Raymond So <rso@ouhk.edu.hk>
2017-05-14 12:33:03 +0800

    Changed function name (from helloworld) to simpleinterest

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ var functions = require('firebase-functions');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.simpleinterest = functions.https.onRequest((request, response) => {
 	function SimpleInterest(P,i,t) {
 		this.principal = P;
 		this.rate = i;
